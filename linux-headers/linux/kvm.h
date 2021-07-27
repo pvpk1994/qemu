493ec7bb50b5020429809cacdbcbad1c5b2f5b65
Ashish Kalra <ashish.kalra@amd.com>
2021-07-27 15:05:49 +0000

    kvm: Add support for SEV shared regions list and KVM_EXIT_HYPERCALL.
    cherry-picked from https://github.com/AMDESE/qemu/commit/fcbbd9b19ac.
    
    KVM_HC_MAP_GPA_RANGE hypercall is used by the SEV guest to notify a
    change in the page encryption status to the hypervisor. The hypercall
    should be invoked only when the encryption attribute is changed from
    encrypted -> decrypted and vice versa. By default all guest pages are
    considered encrypted.
    
    The hypercall exits to userspace with KVM_EXIT_HYPERCALL exit code,
    currently this is used only by SEV guests for guest page encryptiion
    status tracking. Add support to handle this exit and invoke SEV
    shared regions list handlers.
    
    Add support for SEV guest shared regions and implementation of the
    SEV shared regions list.
    
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    [ Fix conflicts. ]
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -346,6 +346,7 @@ struct kvm_run {
 		} iocsr_io;
 		/* KVM_EXIT_HYPERCALL */
 		struct {
+#define KVM_HC_MAP_GPA_RANGE 12
 			__u64 nr;
 			__u64 args[6];
 			__u64 ret;
@@ -1197,6 +1198,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ARM_EAGER_SPLIT_CHUNK_SIZE 228
 #define KVM_CAP_ARM_SUPPORTED_BLOCK_SIZES 229
 #define KVM_CAP_ARM_SUPPORTED_REG_MASK_RANGES 230
+
+#define KVM_EXIT_HYPERCALL_VALID_MASK (1 << KVM_HC_MAP_GPA_RANGE)
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
