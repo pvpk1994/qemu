96e1cc62a2f88bf1c07002667d01db47e27d25be
Bryan Zhang <bryan.zhang@BYTEDANCE.COM>
2024-03-01 03:59:00 +0000

    migration: Properly apply migration compression level parameters
    commit b4014a2bf57ce08e2f6458cd82e9f968facf25c8 upstream.
    
    Some glue code was missing, so that using `qmp_migrate_set_parameters`
    to set `multifd-zstd-level` or `multifd-zlib-level` did not work. This
    commit adds the glue code to fix that.
    
    Intel-SIG: commit b4014a2bf57c migration: Properly apply migration compression level parameters
    
    Signed-off-by: Bryan Zhang <bryan.zhang@bytedance.com>
    Link: https://lore.kernel.org/r/20240301035901.4006936-2-bryan.zhang@bytedance.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- migration/options.c
+++ migration/options.c
@@ -1333,6 +1333,12 @@ static void migrate_params_test_apply(MigrateSetParameters *params,
     if (params->has_multifd_compression) {
         dest->multifd_compression = params->multifd_compression;
     }
+    if (params->has_multifd_zlib_level) {
+        dest->multifd_zlib_level = params->multifd_zlib_level;
+    }
+    if (params->has_multifd_zstd_level) {
+        dest->multifd_zstd_level = params->multifd_zstd_level;
+    }
     if (params->has_xbzrle_cache_size) {
         dest->xbzrle_cache_size = params->xbzrle_cache_size;
     }
@@ -1484,6 +1490,12 @@ static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
     }
     if (params->has_multifd_compression) {
         s->parameters.multifd_compression = params->multifd_compression;
+    }
+    if (params->has_multifd_zlib_level) {
+        s->parameters.multifd_zlib_level = params->multifd_zlib_level;
+    }
+    if (params->has_multifd_zstd_level) {
+        s->parameters.multifd_zstd_level = params->multifd_zstd_level;
     }
     if (params->has_xbzrle_cache_size) {
         s->parameters.xbzrle_cache_size = params->xbzrle_cache_size;
