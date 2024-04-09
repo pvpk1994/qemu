ee242fd623a417791fd43f339c3a5299778f66ad
Michael Roth <michael.roth@amd.com>
2024-04-09 18:07:41 -0500

    i386/sev: Add 'legacy-vm-type' parameter for SEV guest objects
    commit 023267334da375226720e62963df9545aa8fc2fd upstream
    
    QEMU will currently automatically make use of the KVM_SEV_INIT2 API for
    initializing SEV and SEV-ES guests verses the older
    KVM_SEV_INIT/KVM_SEV_ES_INIT interfaces.
    
    However, the older interfaces will silently avoid sync'ing FPU/XSAVE
    state to the VMSA prior to encryption, thus relying on behavior and
    measurements that assume the related fields to be allow zero.
    
    With KVM_SEV_INIT2, this state is now synced into the VMSA, resulting in
    measurements changes and, theoretically, behaviorial changes, though the
    latter are unlikely to be seen in practice.
    
    To allow a smooth transition to the newer interface, while still
    providing a mechanism to maintain backward compatibility with VMs
    created using the older interfaces, provide a new command-line
    parameter:
    
      -object sev-guest,legacy-vm-type=true,...
    
    and have it default to false.
    
    [Backport Changes]
    
    1. In file qapi/qom.json, within the SevGuestProperties struct, the
    addition of the kernel-hashes property was skipped as it already exists
    in the current source code and only legacy-vm-type property was appended
    as per the current upstream patch.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Message-ID: <20240409230743.962513-2-michael.roth@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Gokul K <K.Gokul@amd.com>

--- target/i386/sev.c
+++ target/i386/sev.c
@@ -78,6 +78,7 @@ struct SevGuestState {
     char *user_id;
     char *secret_header_file;
     char *secret_file;
+    bool legacy_vm_type;
 
     /* runtime state */
     uint32_t handle;
@@ -455,6 +456,16 @@ static void sev_guest_set_kernel_hashes(Object *obj, bool value, Error **errp)
     SevGuestState *sev = SEV_GUEST(obj);
 
     sev->kernel_hashes = value;
+}
+
+static bool sev_guest_get_legacy_vm_type(Object *obj, Error **errp)
+{
+    return SEV_GUEST(obj)->legacy_vm_type;
+}
+
+static void sev_guest_set_legacy_vm_type(Object *obj, bool value, Error **errp)
+{
+    SEV_GUEST(obj)->legacy_vm_type = value;
 }
 
 bool
@@ -1105,7 +1116,7 @@ static int sev_kvm_type(X86ConfidentialGuest *cg)
     }
 
     kvm_type = (sev->policy & SEV_POLICY_ES) ? KVM_X86_SEV_ES_VM : KVM_X86_SEV_VM;
-    if (kvm_is_vm_type_supported(kvm_type)) {
+    if (kvm_is_vm_type_supported(kvm_type) && !sev->legacy_vm_type) {
         sev->kvm_type = kvm_type;
     } else {
         sev->kvm_type = KVM_X86_DEFAULT_VM;
@@ -2798,6 +2809,11 @@ sev_guest_class_init(ObjectClass *oc, void *data)
                    sev_guest_set_secret_file);
     object_class_property_set_description(oc, "secret-file",
        "file of the guest owner's secret");
+    object_class_property_add_bool(oc, "legacy-vm-type",
+                                   sev_guest_get_legacy_vm_type,
+                                   sev_guest_set_legacy_vm_type);
+    object_class_property_set_description(oc, "legacy-vm-type",
+            "use legacy VM type to maintain measurement compatibility with older QEMU or kernel versions.");
 }
 
 static void
