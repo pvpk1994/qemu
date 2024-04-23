cd27fb84d74231353a1d953a3f63826fec31714b
Paolo Bonzini <pbonzini@redhat.com>
2024-04-23 11:46:47 +0200

    linux-headers: update to current kvm/next
    commit ab0c7fb22b56523f24d6e127cd4d10ecff67bf85 upstream
    
    [Backport changes]
    
    1. The upstream patch moves the enum sev_cmd_id structure from the
    linux-headers/linux/kvm.h file to the linux-headers/asm-x86/kvm.h file.
    The Hygon-related changes found in the enum sev_cmd_id structure in
    linux-headers/linux/kvm.h have been moved to the enum t sev_cmd_id
    structure in the backported linux-headers/asm-x86/kvm.h file.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: priyanka-mani <DMani.Priyanka@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- linux-headers/asm-loongarch/kvm.h
+++ linux-headers/asm-loongarch/kvm.h
@@ -14,9 +14,7 @@
  * Some parts derived from the x86 version of this file.
  */
 
-#define __KVM_HAVE_READONLY_MEM
 #define __KVM_HAVE_GUEST_DEBUG
-
 #define KVM_COALESCED_MMIO_PAGE_OFFSET	1
 #define KVM_DIRTY_LOG_PAGE_OFFSET	64
 #define __KVM_HAVE_IRQ_LINE
